ad1ca5b104f78b20c9e07f9facb2a867348c10bd
Nathaniel Beaver <nathanielmbeaver@gmail.com>
2015-01-20 02:30:21 -0600

    Changed dump directory to an external directory so I can easily track it separately.

--- wget-groovebackup.sh
+++ wget-groovebackup.sh
@@ -1,8 +1,8 @@
 #! /usr/bin/env bash
 
-mkdir --parents ./groovebackup/
+mkdir --parents ~/backup/grooveshark-backup/
 wget --load-cookies cookies.txt \
 	--output-file=wget.log \
-	--directory-prefix=groovebackup/ \
+	--directory-prefix ~/backup/grooveshark-backup/ \
 	--recursive --convert-links --server-response http://groovebackup.com/home \
 	--reject index.html?lang=de,index.html?lang=dk,index.html?lang=en,logout
